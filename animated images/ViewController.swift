6001f9f55d89e0b42702d8779397ddfd5bca8ac5
gracka92 <gracjana.leonowicz@gmail.com>
2017-09-14 15:15:48 +0200

    first commit

--- animated images/ViewController.swift
+++ animated images/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    var counter = 1
+    var isAnimating = false
+    var timer = Timer()
+    @objc func animate() {
+        image.image = UIImage(named: "frame_\(counter)_delay-0.25s.gif")
+        counter += 1
+        if counter == 5 {
+            counter = 0
+        }
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        if isAnimating {
+            timer.invalidate()
+            button.setTitle("Start Animation", for: [])
+            isAnimating = false
+        } else {
+            timer = Timer.scheduledTimer(timeInterval: 0.25, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
+            button.setTitle("Stop Animation", for: [])
+            isAnimating = true
+        }
+    }
+            
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
